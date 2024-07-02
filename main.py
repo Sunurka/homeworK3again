704e18259bc6ab7315c6886fafd6117a2405820d
Sunurka <sunurka97@mail.ru>
2024-07-02 20:15:54 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+name = 'Name:Сунур'
+print(name)
+age = 26
+print('Age:',age)
+age = 26+1
+print('New age:',age)
+is_student = True
+print('is_student:',is_student)
